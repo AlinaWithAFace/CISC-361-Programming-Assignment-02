d5cb3785d7d75940d80efafef7e333d254b8b9a4
James <jimmyskripchuk@gmail.com>
2018-09-27 20:45:16 -0400

    fixed memory leaks

--- sh.c
+++ sh.c
@@ -11,8 +11,6 @@
 #include <signal.h>
 #include "sh.h"
 
-#define MAX_CANON 32
-#define PATH_MAX 32
 #define BUFFER_SIZE 100
 
 void do_nothing_handler(int sig){
@@ -23,7 +21,7 @@ int sh(int argc, char **argv, char **envp) {
     char BUFFER [BUFFER_SIZE];
 
     char *prompt = calloc(PROMPTMAX, sizeof(char));
-    char *commandline = calloc(MAX_CANON, sizeof(char));
+    //char *commandline = calloc(MAX_CANON, sizeof(char));
     char *command, *arg, *commandpath, *p, *pwd, *owd;
     char **args = calloc(MAXARGS, sizeof(char *));
     int uid, i, status, argsct, go = 1;
@@ -48,11 +46,12 @@ int sh(int argc, char **argv, char **envp) {
 
     /* Put PATH into a linked list */
     pathlist = get_path();
-    //printf("%s", pathlist->next->element);
     
     char prompt_char[BUFFER_SIZE] = "[🔥 👉 😎 👉 🔥] >";
     int len;
     char* string_input;
+
+    //TODo: Segfaults on signal interrupt commmands
     signal(SIGINT, do_nothing_handler);
     signal(SIGTERM, do_nothing_handler);
     signal(SIGTSTP, do_nothing_handler);
@@ -61,33 +60,26 @@ int sh(int argc, char **argv, char **envp) {
         /* print your prompt */
         //todo, fix memory and garbage stuff
 
-        //breaks when you input ntohing
+        //TODO: breaks when you input nothing 
         printf("%s", prompt_char);
         
         fgets(BUFFER, BUFFER_SIZE, stdin);
         len = (int)strlen(BUFFER);
-        //printf("%d", len);
+
         BUFFER[len-1] = '\0';
         string_input = (char*)malloc(len);
         strcpy(string_input, BUFFER);
-        //printf("%s", string_input);
+
         if(string_input != NULL){
             char* token = strtok(string_input, " ");
-            char** args;
-            args = calloc(10, sizeof(char*));
-        //args[0] = token;
             int i = 0;
             while(token){
-            //printf("%s\n",token);
                 len = (int)strlen(token);
                 args[i] = (char*)malloc(len);
                 strcpy(args[i], token);
                 token = strtok(NULL," ");
                 i++;
             }
-        
-
-        //printf("%s", args[0]);
 
             if(strcmp(args[0], "exit") == 0){
                 go = 0;
@@ -97,41 +89,41 @@ int sh(int argc, char **argv, char **envp) {
                 }else{
                     //TODO Impelemnt multiple args
                     char* result = which(args[1], pathlist);
-                    printf("%s\n", result);
+                    if(result != NULL){
+                        printf("%s\n", result);
+                        free(result);
+                    }else{
+                        printf("%s not found\n", args[1]);
+                    }
                 }
             }
-
-        //switch(S)
-
-        
-       // for(int i = 0; i < 10; i++){
-         //   if(args[i] != NULL)
-          //      printf("%s", args[i]);
-        //}
-       
-        //    printf("%s",input_tokenized);
-        //    input_tokenized = strtok(NULL, " ");
-        //}
-
-        //char* token = strtok(BUFFER, " ");    
-        //while(token){
-        //    printf("%s", token);
-        //}
-
-        /* get command line and process */
-
-        /* check for each built in command and implement */
-
-        /*  else  program to exec */
-        //{
-            /* find it */
-            /* do fork(), execve() and waitpid() */
-
-        //    else
-        //    fprintf(stderr, "%s: Command not found.\n", args[0]);
-        //}
+            free(token);
         }
+
+        for(i=0;i<MAXARGS;i++){
+            free(args[i]);
+        }
+        free(string_input);
     }
+
+    //Free ALL the things!
+
+    free(prompt);
+    free(owd);
+    free(args);
+
+    struct pathelement *current;
+    current = pathlist;
+    //Okay what the hell is going on, if you free the first element
+    //It frees like 200 bytes of stuff and if you try to free any
+    //other it breaks????????????? But according to valgrind it's okay?????
+    free(current->element);
+    while(current != NULL){
+        free(current);
+        current=current->next;
+    }
+
+
     return 0;
 } /* sh() */
 
@@ -144,18 +136,22 @@ char *which(char *command, struct pathelement *pathlist) {
     //A buffer for concatting strings
     char CAT_BUFFER[BUFFER_SIZE];
     struct pathelement* current = pathlist;
+
+    DIR *dr;
+    struct dirent *de; 
     
     //Go though all our PATH_ENVs
     while(current != NULL){
+        
         char* path = current->element;
 
         //vars for looking though the directories
-        DIR *dr;
-        struct dirent *de; 
         dr = opendir(path);
+       // printf("Dir closed");
 
         //in each path, look at all of it's files
         while((de = readdir(dr)) != NULL){
+
            
             //for each file in the directory, check if it's the one we want
             if(strcmp(de->d_name, command) == 0){
@@ -169,11 +165,17 @@ char *which(char *command, struct pathelement *pathlist) {
                 char* p = (char*)malloc(len);
                 strcpy(p, CAT_BUFFER);
 
+                closedir(dr);
+
                 return p;
             }
         }
+        closedir(dr);
         current=current->next;
     }
+
+   // free(dr);
+    //free(de);
 
     //Return null if we haven't found one 
     return NULL;
